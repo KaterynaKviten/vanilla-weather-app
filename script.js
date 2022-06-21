b109c2c29db0255cef63d068422bd50e276da026
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2022-06-21 19:12:56 +0300

    add axios

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
 let apiKey = "4826c8e04d4686528238e637fd752385";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
