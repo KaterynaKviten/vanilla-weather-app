b8faa727817053d64fa8df34ec557e15504a2a7b
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2022-06-21 19:53:02 +0300

    added weather element

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#weather-description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let precipitationElement = document.querySelector("#precipitation");
+  precipitationElement.innerHTML = response.data.precipitatio;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
 let apiKey = "4826c8e04d4686528238e637fd752385";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
