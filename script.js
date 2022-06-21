2e984b09cdd94809c589504e43199648f606c91a
Kateryna Shyurkova <katerina.shyurkova@gmail.com>
2022-06-21 20:50:21 +0300

    added date format

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+function formatDate() {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thursday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -7,16 +24,20 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#weather-description");
   descriptionElement.innerHTML = response.data.weather[0].description;
 
-  let precipitationElement = document.querySelector("#precipitation");
-  precipitationElement.innerHTML = response.data.precipitatio;
+  let feelsLikeElement = document.querySelector("#feels-like");
+  feelsLikeElement.innerHTML = Math.round(response.data.main.feels_like);
 
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let dataElement = document.querySelector("#date");
+  dataElement.innerHTML = formatDate();
 }
 
 let apiKey = "4826c8e04d4686528238e637fd752385";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+let city = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
